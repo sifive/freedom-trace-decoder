256e0218c89cc644dd1ccd20f182432614c807da
Kevin Mills <kevin.mills@sifive.com>
2020-10-23 10:55:19 -0700

    back to master, new version number

--- Version.mk
+++ Version.mk
@@ -1,5 +1,5 @@
 # Version number, which should match the official version of the tool we are building
-TRACE_DECODER_VERSION := 0.9.4
+TRACE_DECODER_VERSION := 0.10.0
 
 # Customization ID, which should identify the customization added to the original by SiFive
-FREEDOM_TRACE_DECODER_ID := 2020.09.0-preview1
+FREEDOM_TRACE_DECODER_ID := 2020.10.0-preview1
