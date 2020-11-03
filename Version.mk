0e7b9780ec8fbfa54815272d796ea349b1a9b65f
Kevin Mills <kevin.mills@sifive.com>
2020-11-03 13:41:38 -0800

    bump version id for Nov release

--- Version.mk
+++ Version.mk
@@ -2,4 +2,4 @@
 include src/trace-decoder/version.mk
 
 # Customization ID, which should identify the customization added to the original by SiFive
-FREEDOM_TRACE_DECODER_ID := 2020.10.0-preview1
+FREEDOM_TRACE_DECODER_ID := 2020.11.0
