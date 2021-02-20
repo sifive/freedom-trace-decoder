8846ceed5a8c9948f39eeccd5cc0b57acd5645ea
cgsfv <cgsfv@users.noreply.github.com>
2021-02-20 12:24:46 -0800

    Aligning with 2020.12-release branch

--- Version.mk
+++ Version.mk
@@ -2,8 +2,8 @@
 include src/trace-decoder/version.mk
 
 # Customization ID, which should identify the customization added to the original by SiFive
-FREEDOM_TRACE_DECODER_ID := 2021.01.0-preview1
+FREEDOM_TRACE_DECODER_ID := 2021.01.0-preview2
 
 # Characteristic tags, which should be usable for matching up providers and consumers
-FREEDOM_TRACE_DECODER_RISCV_TAGS = rv32i rv64i m a f d c v zfh
+FREEDOM_TRACE_DECODER_RISCV_TAGS = rv32i rv64i m a f d c v zfh zba zbb
 FREEDOM_TRACE_DECODER_TOOLS_TAGS = trace-decoder
