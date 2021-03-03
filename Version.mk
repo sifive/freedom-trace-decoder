09c92583cde3301062e1161ff82c4549befbbfe1
Kevin Mills <kevin.mills@sifive.com>
2021-03-03 14:33:35 -0800

    prepare release build

--- Version.mk
+++ Version.mk
@@ -2,7 +2,7 @@
 include src/trace-decoder/version.mk
 
 # Customization ID, which should identify the customization added to the original by SiFive
-FREEDOM_TRACE_DECODER_ID := 2021.01.0-preview2
+FREEDOM_TRACE_DECODER_ID := 2021.02.0
 
 # Characteristic tags, which should be usable for matching up providers and consumers
 FREEDOM_TRACE_DECODER_RISCV_TAGS = rv32i rv64i m a f d c v zfh zba zbb
